31dc28c53ae149273495043ebb0ff287b80241aa
manish19202kumae <Manish19202kumar>
2024-04-06 23:24:25 +0530

    first day

---
+++ src/App.js
@@ -0,0 +1,44 @@
+// first method
+// import React from 'react'
+
+// const App = () => {
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+// // rafc
+// export default App
+
+
+//second method
+
+// import React from 'react'
+
+// const App = () => {
+//   return (
+//     <div>
+//       welcome to my web page
+//     </div>
+//   )
+// }
+// // rafce
+// export default App
+
+
+// 3rd method
+
+import React from "react";
+
+function App(){
+  return <h1>Hello world</h1>;
+}
+
+// how react is actully looking into it
+
+
+export default App;
+// we have to export default App or something is mandatory
+// export is used in index.js as import statement
+
